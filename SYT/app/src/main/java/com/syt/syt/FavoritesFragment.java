cc68ceacc656f0d53217e2e48cc530f5a95a18bb
Marina Gabriela Silva <mgabrs@Marina.local>
2015-09-25 15:47:19 -0300

    Adding favorites fragment

--- SYT/app/src/main/java/com/syt/syt/FavoritesFragment.java
+++ SYT/app/src/main/java/com/syt/syt/FavoritesFragment.java
@@ -1,10 +1,22 @@
 package com.syt.syt;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 
 /**
  * Created by mgabrs on 25/09/15.
  */
 public class FavoritesFragment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_favorites, container, false);
+        return rootView;
+    }
+
+
 }
